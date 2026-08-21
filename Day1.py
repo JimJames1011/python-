6b14bc9f4c105e37b973ad1fc9bc5423efa1cb11
JimJames1011 <1330236966@qq.com>
2026-08-21 14:45:59 +0800

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,20 @@
+"""name="Jim"
+age=19
+money=47800
+#通过print语句输出字面量
+print(name)
+print(age+1)
+print("Jim的money还有",money)
+money=money+100000
+print("Jim的money还有",money)"""
+
+
+#练习 求钱包余额 初始金额50
+money=50
+print("当前钱包余额为",money)
+#购买冰淇淋10元 可乐5元
+cream=10
+coke=5
+money=money-(cream+coke)
+#最后总余额
+print("最终余额为",money)
