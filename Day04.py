549e63c08c6d8c2607523115716397ba77abb45a
JimJames1011 <1330236966@qq.com>
2026-08-24 14:19:10 +0800

    布尔类型与比较运算符

---
+++ Day04.py
@@ -0,0 +1,22 @@
+"""name=input("Enter your name:")
+age=int(input("Enter your age:"))
+height=int(input("Enter your height"))
+print(f"My name is {name} and I am {age} years old and I'm {height} cm")"""
+
+
+"""#定义变量储存布尔类型的数据
+bool_1=True
+bool_2=False
+print("bool_1",type(bool_1))"""
+
+
+#比较运算符的使用
+result=10>5
+print(f"{result}",type(result))
+num1=10
+num2=10
+print(f"10==10的结果是{num1==num2}")
+#！=是不等于的意思
+num1=10
+num2=5
+print(f"10!=5的结果是{num1!=num2}")
